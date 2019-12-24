7606a26a65c9fc518a87403edf387a7de453dad5
Andrew Parnham <atparnham@gmail.com>
2019-12-24 12:21:12 +0000

    Adding a basic eventing implementation

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -1,12 +1,27 @@
 interface UserProps {
-  name: string;
-  age: number;
+  name?: string;
+  age?: number;
 }
 
+type EventCallback = () => void;
+
 export class User {
+  private events: {[eventName: string]: EventCallback[]} = {};
+
   constructor(private data: UserProps) {}
 
-  public get(property: string): (string | number) {
+  public get(property: string): string | number {
     return this.data[property];
+  }
+
+  public set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  public on(eventName: string, callback: EventCallback):void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+
+    this.events[eventName] = handlers;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,7 @@ import { User } from "./Models/User";
 
 const user = new User({ name: "Andrew", age: 26 });
 
+user.set({ name: "Trevor", age: 999 });
+
 console.log("Name: " + user.get("name"));
+console.log("Age: " + user.get("age"));
