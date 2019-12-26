e3e1c103ed83ab80f48c05a27f9d616a66ed27cd
Andrew Parnham <atparnham@gmail.com>
2019-12-26 11:38:35 +0000

    Convert functions to arrow functions to fix this binding

--- src/Models/Core/Events/Eventing.ts
+++ src/Models/Core/Events/Eventing.ts
@@ -3,14 +3,14 @@ type EventCallback = () => void;
 export class Eventing {
   private events: { [eventName: string]: EventCallback[] } = {};
 
-  public on(eventName: string, callback: EventCallback): void {
+  public on = (eventName: string, callback: EventCallback): void => {
     const handlers = this.events[eventName] || [];
     handlers.push(callback);
 
     this.events[eventName] = handlers;
   }
 
-  public trigger(eventName: string): void {
+  public trigger = (eventName: string): void => {
     const handlers = this.events[eventName];
 
     if (!handlers || handlers.length === 0) {
